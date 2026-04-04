c4209c1a66b180c11840a4bd5b425d63f0fd9e4e
Afonso Santos <afonso.santos.2025078@my.istec.pt>
2026-04-04 23:29:08 +0100

    Implementação do sistema de submissão e leitura de comentários

--- noticias/urls.py
+++ noticias/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Rota para a página inicial (lista de artigos)
     path('', views.lista_artigos, name='lista_artigos'),
-    # Rota para o detalhe de um artigo específico
     path('artigo/<int:artigo_id>/', views.detalhe_artigo, name='detalhe_artigo'),
+    
+    # --- NOVA ROTA DA FASE 4 ---
+    path('artigo/<int:artigo_id>/comentarios/', views.comentarios_artigo, name='comentarios_artigo'),
 ]
--- noticias/views.py
+++ noticias/views.py
@@ -1,14 +1,34 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Artigo
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Artigo, Comentario
 
 def lista_artigos(request):
-    # Vai buscar todos os artigos à base de dados
     artigos = Artigo.objects.all()
-    # Envia os artigos para o template 'lista_artigos.html'
     return render(request, 'noticias/lista_artigos.html', {'artigos': artigos})
 
 def detalhe_artigo(request, artigo_id):
-    # Vai buscar o artigo específico pelo ID ou devolve um erro 404 se não existir
     artigo = get_object_or_404(Artigo, id=artigo_id)
-    # Envia o artigo para o template 'detalhe_artigo.html'
     return render(request, 'noticias/detalhe_artigo.html', {'artigo': artigo})
+
+# --- NOVA FUNÇÃO DA FASE 4 ---
+def comentarios_artigo(request, artigo_id):
+    artigo = get_object_or_404(Artigo, id=artigo_id)
+    
+    # Se o utilizador submeteu o formulário (POST)
+    if request.method == 'POST':
+        # Vai buscar o texto que foi escrito no formulário HTML
+        texto = request.POST.get('texto_comentario')
+        
+        # Se o texto não estiver vazio, cria e guarda o comentário
+        if texto:
+            Comentario.objects.create(artigo=artigo, texto_comentario=texto)
+            # Recarrega a página para mostrar o novo comentário
+            return redirect('comentarios_artigo', artigo_id=artigo.id)
+    
+    # Se for apenas para ver a página (GET)
+    # Vai buscar todos os comentários ligados a este artigo específico
+    comentarios = artigo.comentarios.all()
+    
+    return render(request, 'noticias/comentarios_artigo.html', {
+        'artigo': artigo, 
+        'comentarios': comentarios
+    })
